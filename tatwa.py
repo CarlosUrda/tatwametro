05584fc0ad223e8b190a18bddbd70faada9e07ec
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2017-08-05 22:04:34 +0200

    Finalización de las primeras pruebas para la clase Tatwa

--- tatwa.py
+++ tatwa.py
@@ -18,68 +18,176 @@ class Tatwa:
     NOMBRES_TATWAS = ("akash", "vayu", "tejas", "prithvi", "apas") 
     
 
-    def __init__(self, tatwa):
+    def __init__(self, tatwa, ciclo=None):
         """
         Constructor.
 
         Argumentos:
-            tatwa: nombre del tatwa o posición (>= 1) del tatwa en el 
-                ciclo de tatwas.
+            tatwa: nombre del tatwa (str) o posición (int >= 1) del 
+                tatwa en el ciclo de tatwas.
+            ciclo: número (int >= 1) del ciclo al cual está asociado 
+                este tatwa. None si no se quiere considerar el número
+                de ciclo para este tatwa.
+                Si el primer parámetro tatwa es una posición, el 
+                ciclo es calculado a partir de la misma, siendo el 
+                parámetro ciclo ignorado.
 
         Excepciones:
-            TypeError si el argumento no es str o int.
-            ValuError si la posición no es > 0. 
-
+            TypeError si los tipos de los argumentos no son permitidos.
+            ValueError si el valor de los argumentos no son permitidos.
         """
-        if isinstance(tatwa, int):
-            if tatwa < 1:
-                raise ValueError("La posición debe ser > 0")
-            self._nombre_tatwa = NOMBRES_TATWAS[(tatwa - 1) 
-                                                % len(NOMBRES_TATWAS)]
+        try:
+            self.cambiar_tatwa_por_posicion(tatwa)
+        except TypeError:
+            pass
         else:
-            self.nombre_tatwa = nombre_tatwa
+            return
+
+        try:
+            self.nombre = tatwa
+        except TypeError:
+            raise TypeError("El primer argumento debe ser str o int")
+    
+        self.ciclo = ciclo
         
 
     @property
-    def nombre_tatwa(self):
+    def nombre(self):
         """
-        Getter del atributo nombre_tatwa
+        Getter del atributo nombre
         """
-        return self._nombre_tatwa
+        return self._nombre
 
-    @tatwa.setter
-    def nombre_tatwa(self, nombre_tatwa):
+    @nombre.setter
+    def nombre(self, nombre):
         """
         Setter del atributo nombre_tatwa
+
+        Argumentos:
+            nombre: nombre del tatwa a asignar.
+
+        Excepciones:
+            TypeError si el argumento no es de tipo str.
+            ValueError si el nombre del tatwa no es uno de los nombres
+                válidos.
         """
-        if not isinstance(nombre_tatwa, str):
+        if not isinstance(nombre, str):
             raise TypeError("El nombre del tatwa debe ser un str.")
 
-        nombre_tatwa = nombre_tatwa.lower()
-        if nombre_tatwa not in NOMBRES_TATWAS:
+        nombre = nombre.strip().lower()
+        if nombre not in self.NOMBRES_TATWAS:
             raise ValueError("Valor incorrecto del nombre del tatwa")
-        self._nombre_tatwa = nombre_tatwa
 
-    
+        self._nombre = nombre
+
+
+    @property
+    def ciclo(self):
+        """
+        Getter del atributo ciclo.
+        """
+        return self._ciclo
+
+    @ciclo.setter
+    def ciclo(self, ciclo):
+        """
+        Setter del atributo ciclo.
+
+        Argumentos:
+            ciclo: número entero int del ciclo a asignar al tatwa.
+                None si no se desea considerar ningún número de ciclo
+                para este tatwa. 
+
+        Excepciones:
+            TypeError si el ciclo no es un entero int o None.
+            ValueError si el ciclo int es < 1.
+        """
+        if isinstance(ciclo, int):
+            if ciclo < 1:
+                raise ValueError("El valor entero de ciclo debe ser >= 1")
+        elif ciclo is not None:
+            raise TypeError("El ciclo debe ser un entero o None.")
+
+        self._ciclo = ciclo
+
+
+    def cambiar_tatwa_por_posicion(self, posicion):
+        """
+        Cambiar el tatwa por el correspondiente a la nueva posición
+        en el ciclo de tatwas (empezando por 1). También modifica
+        el número de ciclo en función de la posición indicada.
+
+        Argumentos:
+            posicion: posición (>= 1) del tatwa en el ciclo de tatwas.
+
+        Excepciones:
+            TypeError si el argumento no es un entero int.
+            ValueError si la posición no es > 0. 
+        """
+        if not isinstance(posicion, int):
+            raise TypeError("La nueva posición debe ser un entero int.")
+
+        if posicion < 1:
+            raise ValueError("La nueva posición debe ser > 0.")
+
+        numero_de_tatwas = len(self.NOMBRES_TATWAS)
+        indice = (posicion - 1) % numero_de_tatwas
+        self._ciclo = (posicion - 1) // numero_de_tatwas + 1       
+        self._nombre = self.NOMBRES_TATWAS[indice]
+     
+
+    @classmethod
+    def _indice_de_nombre_tatwa(cls, nombre):
+        """
+        Obtener el índice en el primer ciclo de tatwas asociado al
+        correspondiente nombre de un tatwa (empezando por 0).
+
+        Argumentos:
+            nombre: nombre del tatwa en minúsculas.
+
+        Retorno:
+            Índice del ciclo correspondiente al nombre del tatwa.
+
+        Excepciones:
+            ValueError si el nombre no es de ningún tatwa.
+        """
+        try:
+            return cls.NOMBRES_TATWAS.index(nombre)
+        except ValueError:
+            raise ValueError("El nombre no corresponde a ningún tatwa")
+
+
     def _indice(self):
         """
-        Obtener el índice del tatwa en el el ciclo de los tatwas.
+        Obtener el índice del tatwa en el primer ciclo de los tatwas
+        (empezando por 0).
+        
+        Retorno:
+            Entero índice del tatwa (>= 0) en el ciclo de tatwas.
         """
-        return NOMBRES_TATWAS.index(self._nombre_tatwa)
+        return self._indice_de_nombre_tatwa(self.nombre)
 
 
     @property
     def posicion(self):
         """
-        Getter para obtener el número de posición en el ciclo de
-        los tatwas.
+        Getter para obtener el número de posición del tatwa en los 
+        ciclos de los tatwas. 
+
+        Retorno:
+            Número de posición en función del ciclo asignado al 
+            tatwa. Si no tiene ciclo, se toma el primero.
         """
-        return self._indice() + 1
+        indice_ciclo = 0 if self.ciclo is None else self.ciclo - 1
+        return (self._indice() + 1) + indice_ciclo * len(self.NOMBRES_TATWAS)
 
 
     def __str__(self):
-        return self._nombre_tatwa.capitalize()
+        return self._nombre.capitalize()
         
+    def __repr__(self):
+        return "{}, {} ({})".format(self, self.posicion, self.ciclo) 
+
 
     def distancia(self, tatwa):
         """
@@ -87,29 +195,32 @@ class Tatwa:
         tatwas.
 
         Argumento:
-            tatwa: Tatwa o nombre de tatwa a calcular la diferencia de
-                posiciones con el tatwa self.
+            tatwa: Objeto Tatwa, nombre de un tatwa (str) o posición
+                de un tatwa en el ciclo (int). En caso de ser un nombre
+                se considera su posición en el primer ciclo.
 
         Retorno:
             Número de posiciones existentes entre los dos tatwas. 
                 Partiendo del tatwa self se cuenta el número de 
-                posiciones existentes hasta el tatwa parámetro.
+                posiciones existentes hasta el tatwa asociado al
+                parámetro.
 
         Excepciones:
             TypeError si el argumento no es un Tatwa o un str.
             ValueError si el argumento es str pero no es un nombre de
                 tatwa válido.
         """
+        if isinstance(tatwa, int):
+            if tatwa < 1:
+                raise ValueError("La posición debe ser > 0")
+            return tatwa - self.posicion
         if isinstance(tatwa, Tatwa):
-            return tatwa.indice() - self._indice() 
+            return tatwa.posicion - self.posicion
         if isinstance(tatwa, str):
-            try:
-                indice = NOMBRES_TATWAS.index(tatwa)
-            except ValueError:
-                raise ValueError("Nombre de argumento tatwa incorrecto.")
-            else:
-                return indice - self._indice()
-        raise TypeError("Tatwa comparado con un tipo de dato incorrecto.")
+            indice = self._indice_de_nombre_tatwa(tatwa.strip().lower())
+            return (indice + 1) - self.posicion
+        
+        raise TypeError("Tipo de dato del argumento incorrecto.")
 
    
     # Operadores de suma y resta 
@@ -118,21 +229,36 @@ class Tatwa:
         """
         Operador suma para obtener el tatwa de una posición concreta
         relativa a partir de este tatwa. 
+        Si el tatwa tiene ciclo, la nueva posición debe ser > 1, ya
+        que al tener en cuenta los ciclos no hay tatwas de posición
+        menor a 1 (anteriores al primer ciclo).
+        Si el tatwa no tiene ciclo, el tatwa resultante tampoco lo 
+        tendrá y la nueva posición puede ser < 1, a la cual se le 
+        aplicará el operado % para obtener una posición siempre
+        entre 1 y número de tatwas.
         
         Argumentos:
             entero: posición relativa del siguente tatwa a obtener.
 
         Retorno:
             Tatwa con la posición relativa correspondiente al entero
-            a partir del tatwa self.
+            a partir del tatwa self. Si el tatwa self no tiene ciclo
+            el tatwa resultado tampoco tendrá ciclo (None).
         """
         if not isinstance(entero, int):
             return NotImplemented
         
-        indice = (self._indice() + entero) % len(NOMBRES_TATWAS)
-        return Tatwa(NOMBRES_TATWAS[indice])
+        if self.ciclo is None:
+            indice = (self._indice() + entero) % len(self.NOMBRES_TATWAS)
+            tatwa_resultado = Tatwa(self.NOMBRES_TATWAS[indice])
+        else:
+            tatwa_resultado = Tatwa(self.posicion + entero)
+
+        return tatwa_resultado
+
 
     __radd__ = __add__
+
 
     def __sub__(self, entero):
         """
@@ -152,30 +278,94 @@ class Tatwa:
     # Operadores de comparación
 
     def __eq__(self, tatwa):
+        """
+        Operador == para comparar tatwas.
+
+        Argumentos:
+            tatwa: puede ser un objeto Tatwa, una posición de un tatwa
+                o el nombre de un tatwa. Si es un objeto sin ciclo o
+                un nombre, se les consideran del primer ciclo.
+
+        Retorno:
+            True/False dependiendo si ambos se refieren al mismo tatwa 
+            en el mismo ciclo.
+        """
         try:
             return self.distancia(tatwa) == 0
         except TypeError:
             return NotImplemented
 
+
     def __gt__(self, tatwa):
+        """
+        Operador > para comparar tatwas.
+
+        Argumentos:
+            tatwa: puede ser un objeto Tatwa, una posición de un tatwa
+                o el nombre de un tatwa. Si es un objeto sin ciclo o
+                un nombre, se les consideran del primer ciclo.
+
+        Retorno:
+            True/False dependiendo si el primer tatwa tiene una 
+            posición mayor al segundo.
+        """
         try:
             return self.distancia(tatwa) < 0
         except TypeError:
             return NotImplemented
 
+
     def __ge__(self, tatwa):
+        """
+        Operador >= para comparar tatwas.
+
+        Argumentos:
+            tatwa: puede ser un objeto Tatwa, una posición de un tatwa
+                o el nombre de un tatwa. Si es un objeto sin ciclo o
+                un nombre, se les consideran del primer ciclo.
+
+        Retorno:
+            True/False dependiendo si el primer tatwa tiene una 
+            posición mayor o igual al segundo.
+        """
         try:
             return self.distancia(tatwa) <= 0
         except TypeError:
             return NotImplemented
 
+
     def __lt__(self, tatwa):
+        """
+        Operador < para comparar tatwas.
+
+        Argumentos:
+            tatwa: puede ser un objeto Tatwa, una posición de un tatwa
+                o el nombre de un tatwa. Si es un objeto sin ciclo o
+                un nombre, se les consideran del primer ciclo.
+
+        Retorno:
+            True/False dependiendo si el primer tatwa tiene una 
+            posición menor al segundo.
+        """
         try:
             return self.distancia(tatwa) > 0
         except TypeError:
             return NotImplemented
 
+
     def __le__(self, tatwa):
+        """
+        Operador <= para comparar tatwas.
+
+        Argumentos:
+            tatwa: puede ser un objeto Tatwa, una posición de un tatwa
+                o el nombre de un tatwa. Si es un objeto sin ciclo o
+                un nombre, se les consideran del primer ciclo.
+
+        Retorno:
+            True/False dependiendo si el primer tatwa tiene una 
+            posición menor o igual al segundo.
+        """
         try:
             return self.distancia(tatwa) >= 0
         except TypeError:
@@ -189,28 +379,9 @@ class EntornoTatwas:
     de tatwas
     """
     
-    _EVENTOS_SOL_ING_ESP = \
-        {"sunrise": "salida", "sunset": "puesta", "solar_noon": "mediodia", 
-         "civil_twilight_begin": "amanecer_civil",
-         "civil_twilight_end": "ocaso_civil",
-         "nautical_twilight_begin": "amanecer_nautico",
-         "nautical_twilight_end": "ocaso_nautico",
-         "astronomical_twilight_begin": "amanecer_astronomico",
-         "astronomical_twilight_end": "ocaso_astronomico"}
-
     _EVENTOS_SOL_PARA_TATWAS = ("salida", "amanecer_civil", 
                                 "amanecer_nautico", "amanecer_astronomico")
 
-    EVENTOS_SOL_DESCRIPCION = \
-        {"salida": "salida del sol", "puesta": "puesta del sol",
-         "mediodia": "sol del mediodía", 
-         "amanecer_civil": "inicio del amanecer civil",
-         "ocaso_civil": "fin del ocaso civil", 
-         "amanecer_nautico": "inicio del amanecer náutico",
-         "ocaso_nautico": "fin del ocaso náutico", 
-         "amanecer_astronomico": "inicio del amanecer astronómico",
-         "ocaso_astronomico": "fin del ocaso astronómico"} 
-    
 
     def __init__(self):
         """
@@ -232,9 +403,10 @@ class EntornoTatwas:
 
         Retorno:
             Devuelve la localización en el siguiente formato:
-            dirección (región).
+            dirección (región). None si no se ha fijado aún la
+            localización.
         """
-        if self._direccion is None
+        if self._direccion is None:
             return None
 
         region = " ({})".format(self._region) if self._region else ""
@@ -278,7 +450,11 @@ class EntornoTatwas:
         Excepciones:
             RuntimeError en caso de haber error al intentar obtener
             las coordenas.
+            ValueError si dirección tiene un valor vacío.
         """
+        if not direccion:
+            raise ValueError("Valor de dirección vacío")
+
         try:
             coordenadas = util.obtener_coordenadas(direccion, region)
         except RuntimeError as err:
@@ -301,7 +477,7 @@ class EntornoTatwas:
             Tupla con el par de valores (latitud, longitud) o None si
             las coordenas no han sido fijadas aún.
         """
-        if self._coordenadas is None
+        if self._coordenadas is None:
             return None
         else:
             return self._coordenadas["lat"], self._coordenadas["lng"]
@@ -377,7 +553,7 @@ class EntornoTatwas:
                 continue
             if not isinstance(hora, dt.time):
                 self._horas_eventos_sol = None
-                raise TypeError("La hora {} no es datetime.time".format(evento)
+                raise TypeError("Hora {} no es datetime.time".format(evento))
             
             self._horas_eventos_sol[evento] = hora
 
@@ -404,7 +580,7 @@ class EntornoTatwas:
             raise ValueError("No se han fijado las coordenadas")
     
         try:
-            horas_eventos_sol = 
+            horas_eventos_sol = \
                 util.obtener_horas_eventos_sol(self._coordenadas["lat"], 
                                                self._coordenadas["lng"], 
                                                self._fecha)
@@ -416,8 +592,8 @@ class EntornoTatwas:
                 raise RuntimeError("La lista obtenida de horas de los eventos"
                                    " del sol está vacía.")
               
-        self._horas_eventos_sol = \ 
-            {_EVENTOS_SOL_ING_ESP[evento_ing]: hora 
+        self._horas_eventos_sol = \
+            {util.EVENTOS_SOL_ING_ESP[evento_ing]: hora 
              for evento_ing, hora in horas_eventos_sol.items()}
 
 
@@ -433,7 +609,7 @@ class EntornoTatwas:
         if self._hora is None:
             raise ValueError("No se ha asignado una hora a calcular.")
         elif self._horas_eventos_sol is None:
-            raise ValueError("No se han obtenido las horas de eventos del sol"))
+            raise ValueError("No se han obtenido las horas de eventos del sol")
     
         self._tatwas = dict()
 
@@ -491,7 +667,7 @@ class EntornoTatwas:
                 print(err)
                 raise TypeError("Error al crear tipo datetime.date")
             
-         self._horas_eventos_sol = None
+        self._horas_eventos_sol = None
 
 
     @property
